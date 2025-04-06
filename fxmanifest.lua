95d17f57b812ccf0231a59e113d8c9b2bdcb1850
CX Scripts <166239193+Centooo34@users.noreply.github.com>
2025-04-06 19:01:05 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,21 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 "yes"
+
+author 'Cento'
+description 'Car clean script'
+
+shared_scripts {
+    "@es_extended/imports.lua"
+}
+
+client_scripts {
+    '@ox_lib/init.lua',
+    'client/client.lua'
+}
+
+
+dependencies {
+    'ox_lib',
+    'es_extended'
+}
