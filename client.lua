bf0c2cfbf39de37a99fd33f28c0051a31b67845a
Jacob <79019386+jacobdev-fivem@users.noreply.github.com>
2025-04-06 19:36:44 +0200

    Anti skip progressbar

--- client.lua
+++ client.lua
@@ -3,28 +3,27 @@ RegisterNetEvent('cleanVehicle', function()
     local includePlayerVehicle = false
     local auto = lib.getClosestVehicle(coords, 1.5, includePlayerVehicle)
 
-    lib.progressBar({
-    duration = 10000,
-    label = 'Cleaning vehicle...', -- you can edit this
-    useWhileDead = false,
-    canCancel = false,
-    disable = {
-        car = true,
-    },
-    anim = {
-        dict = 'amb@world_human_maid_clean@',
-        clip = 'base'
-    },
-    prop = {
-        model = "prop_rag_01",
-        pos = vec3(0.1, 0.0, 0.0),      
-        rot = vec3(0.0, 0.0, 0.0),
-        bone = 57005 
-    }
-})
-
-
-    SetVehicleDirtLevel(auto, 0.0)
+    if lib.progressBar({
+            duration = 10000,
+            label = 'Cleaning vehicle...', -- you can edit this
+            useWhileDead = false,
+            canCancel = false,
+            disable = {
+                car = true,
+            },
+            anim = {
+                dict = 'amb@world_human_maid_clean@',
+                clip = 'base'
+            },
+            prop = {
+                model = "prop_rag_01",
+                pos = vec3(0.1, 0.0, 0.0),
+                rot = vec3(0.0, 0.0, 0.0),
+                bone = 57005
+            }
+        }) then
+        SetVehicleDirtLevel(auto, 0.0)
+    end
 end)
 
 exports.ox_target:addGlobalVehicle({
