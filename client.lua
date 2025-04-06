3f2d9100086977dd3390694a54c935f841666f41
CX Scripts <166239193+Centooo34@users.noreply.github.com>
2025-04-06 19:55:33 +0200

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,36 +1,39 @@
-RegisterNetEvent('cleanVehicle', function()
-    local coords = GetEntityCoords(cache.ped)
-    local includePlayerVehicle = false
-    local auto = lib.getClosestVehicle(coords, 1.5, includePlayerVehicle)
-
-    lib.progressBar({
-    duration = 10000,
-    label = 'Cleaning vehicle...', -- you can edit this
-    useWhileDead = false,
-    canCancel = false,
-    disable = {
-        car = true,
-    },
-    anim = {
-        dict = 'amb@world_human_maid_clean@',
-        clip = 'base'
-    },
-    prop = {
-        model = "prop_rag_01",
-        pos = vec3(0.1, 0.0, 0.0),      
-        rot = vec3(0.0, 0.0, 0.0),
-        bone = 57005 
-    }
-})
-
-
-    SetVehicleDirtLevel(auto, 0.0)
-end)
+local function cleanVehicle(car)
+    if lib.progressBar({
+            duration = 10000,
+            label = 'Cleaning vehicle...', -- you can edit this
+            useWhileDead = false,
+            canCancel = false,
+            allowRagdoll = false,
+            allowSwimming = false,
+            allowCuffed = false,
+            allowFalling = false,
+            disable = {
+                car = true,
+                move = true,
+                combat = true
+            },
+            anim = {
+                dict = 'amb@world_human_maid_clean@',
+                clip = 'base'
+            },
+            prop = {
+                model = "prop_rag_01",
+                pos = vec3(0.1, 0.0, 0.0),
+                rot = vec3(0.0, 0.0, 0.0),
+                bone = 57005
+            }
+        })
+    then
+        SetVehicleDirtLevel(car, 0.0)
+    end
+end
 
 exports.ox_target:addGlobalVehicle({
-    event = "cleanVehicle",
     distance = 1.5,
     icon = "fas fa-water",
-    label = "Clean vehicle"
+    label = "Clean vehicle",
+    onSelect = function(data)
+        cleanVehicle(data.entity)
+    end
 })
-
