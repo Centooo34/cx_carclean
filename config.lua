766d2107e6206467891229391bcc8e3bbcbac397
CX Scripts <166239193+Centooo34@users.noreply.github.com>
2025-04-08 20:11:52 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -2,7 +2,6 @@ Config = {}
 
 Config = {
     Label = "Cleaning vehicle...", -- progress bar label
-    Duration = 10000,
     TargetLabel = "Clean vehiclee",
     RequiredItem = "cleaningtools",
 }
